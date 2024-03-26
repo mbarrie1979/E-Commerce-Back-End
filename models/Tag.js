46c3656e305007015cc5243b28a0b72448ea54b7
Mark Barrie <mark.a.barrie@gmail.com>
2024-03-26 16:26:29 -0400

    finished Models and associations

--- models/Tag.js
+++ models/Tag.js
@@ -2,11 +2,22 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
-    // define columns
+    // Define an id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Define a product_name column
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
